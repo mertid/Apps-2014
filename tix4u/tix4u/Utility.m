a890c2c8b976ddda035a9e7ce75ca44c7f26c39c
Merritt Tidwell <Merritt.tidwell@gmail.com>
2014-09-28 16:39:05 -0400

    Added utility FB methods
    processFacebookProfilePictureData
    userHasValidFacebookData

--- tix4u/tix4u/Utility.m
+++ tix4u/tix4u/Utility.m
@@ -7,7 +7,66 @@
 //
 
 #import "Utility.h"
+#import "Constants.h"
+#import "UIImage+ResizeAdditions.h"
 
 @implementation Utility
+
+#pragma mark Facebook
+
++ (void)processFacebookProfilePictureData:(NSData *)newProfilePictureData {
+    if (newProfilePictureData.length == 0) {
+        return;
+    }
+    
+    // The user's Facebook profile picture is cached to disk. Check if the cached profile picture data matches the incoming profile picture. If it does, avoid uploading this data to Parse.
+    
+    NSURL *cachesDirectoryURL = [[[NSFileManager defaultManager] URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] lastObject]; // iOS Caches directory
+    
+    NSURL *profilePictureCacheURL = [cachesDirectoryURL URLByAppendingPathComponent:@"FacebookProfilePicture.jpg"];
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[profilePictureCacheURL path]]) {
+        // We have a cached Facebook profile picture
+        
+        NSData *oldProfilePictureData = [NSData dataWithContentsOfFile:[profilePictureCacheURL path]];
+        
+        if ([oldProfilePictureData isEqualToData:newProfilePictureData]) {
+            return;
+        }
+    }
+    
+    UIImage *image = [UIImage imageWithData:newProfilePictureData];
+    
+    UIImage *mediumImage = [image thumbnailImage:280 transparentBorder:0 cornerRadius:0 interpolationQuality:kCGInterpolationHigh];
+    UIImage *smallRoundedImage = [image thumbnailImage:64 transparentBorder:0 cornerRadius:9 interpolationQuality:kCGInterpolationLow];
+    
+    NSData *mediumImageData = UIImageJPEGRepresentation(mediumImage, 0.5); // using JPEG for larger pictures
+    NSData *smallRoundedImageData = UIImagePNGRepresentation(smallRoundedImage);
+    
+    if (mediumImageData.length > 0) {
+        PFFile *fileMediumImage = [PFFile fileWithData:mediumImageData];
+        [fileMediumImage saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (!error) {
+                [[PFUser currentUser] setObject:fileMediumImage forKey:kPAPUserProfilePicMediumKey];
+                [[PFUser currentUser] saveEventually];
+            }
+        }];
+    }
+    
+    if (smallRoundedImageData.length > 0) {
+        PFFile *fileSmallRoundedImage = [PFFile fileWithData:smallRoundedImageData];
+        [fileSmallRoundedImage saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (!error) {
+                [[PFUser currentUser] setObject:fileSmallRoundedImage forKey:kPAPUserProfilePicSmallKey];
+                [[PFUser currentUser] saveEventually];
+            }
+        }];
+    }
+}
+
++ (BOOL)userHasValidFacebookData:(PFUser *)user {
+    NSString *facebookId = [user objectForKey:kPAPUserFacebookIDKey];
+    return (facebookId && facebookId.length > 0);
+}
 
 @end
