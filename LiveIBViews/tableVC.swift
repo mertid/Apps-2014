9d2b2fd14441b151fc4a8909755fded0b3880bc0
Merritt Tidwell <Merritt.tidwell@gmail.com>
2014-09-28 15:03:27 -0400

    LIV- Work in progress re live dynamic views in IB
    Mini revision with Heidi

---
+++ LiveIBViews/tableVC.swift
@@ -0,0 +1,45 @@
+//
+//  tableVC.swift
+//  LiveIBViews
+//
+//  Created by Merritt Tidwell on 9/26/14.
+//  Copyright (c) 2014 Merritt Tidwell. All rights reserved.
+//
+
+import UIKit
+
+class tableVC: UITableViewController {
+
+    let items: [String] = ["hey", "what up", "you suck"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as TableViewCell
+
+        // Configure the cell...
+
+        cell.main //?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+
+}
