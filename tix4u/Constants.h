9081248ee2772873a8009c527c637c233b9327a0
Merritt Tidwell <Merritt.tidwell@gmail.com>
2014-10-08 13:44:04 -0400

    adding missing constants

---
+++ tix4u/Constants.h
@@ -0,0 +1,21 @@
+//
+//  Constants.h
+//  tix4u
+//
+//  Created by Merritt Tidwell on 9/28/14.
+//  Copyright (c) 2014 Merritt Tidwell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - PFObject User Class
+// Field keys
+extern NSString *const kPAPUserDisplayNameKey;
+extern NSString *const kPAPUserFacebookIDKey;
+extern NSString *const kPAPUserPhotoIDKey;
+extern NSString *const kPAPUserProfilePicSmallKey;
+extern NSString *const kPAPUserProfilePicMediumKey;
+
+@interface Constants : NSObject
+
+@end
---
+++ tix4u/Constants.m
@@ -0,0 +1,24 @@
+//
+//  Constants.m
+//  tix4u
+//
+//  Created by Merritt Tidwell on 9/28/14.
+//  Copyright (c) 2014 Merritt Tidwell. All rights reserved.
+//
+
+#import "Constants.h"
+
+#pragma mark - User Class
+// Field keys
+NSString *const kPAPUserDisplayNameKey                          = @"displayName";
+NSString *const kPAPUserFacebookIDKey                           = @"facebookId";
+NSString *const kPAPUserPhotoIDKey                              = @"photoId";
+NSString *const kPAPUserProfilePicSmallKey                      = @"profilePictureSmall";
+NSString *const kPAPUserProfilePicMediumKey                     = @"profilePictureMedium";
+
+@implementation Constants
+
+@end
+
+
+
